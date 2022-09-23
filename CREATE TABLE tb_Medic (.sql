f5d074dd69e4f938fe3eb58d2f6f544544b92aa7
thapL <suraipaza@gmail.com>
2022-09-23 16:49:36 +0700

    add sql comman

---
+++ CREATE TABLE tb_Medic (.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_Medic (
+    Medic_name  NVARCHAR(150) NOT NULL,
+    Medic_quan  int NULL,
+    Medic_Unit  NVARCHAR(50) NULL,
+    Medic_Id int not null PRIMARY KEY
+
+);
---
+++ SELECT TOP (1000) [Medic_name].sql
@@ -0,0 +1,5 @@
+SELECT TOP (1000) [Medic_name]
+      ,[Medic_quan]
+      ,[Medic_Unit]
+      ,[Medic_Id]
+  FROM [MediCin].[dbo].[tb_Medic]
